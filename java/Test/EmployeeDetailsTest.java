e1e10ef18737c885915ded48d858c176feea5af9
OisinODonnell <oisinodonnell@hotmail.com>
2017-03-08 10:37:37 +0000

    EmployeeDetailsTest Class

---
+++ java/Test/EmployeeDetailsTest.java
@@ -0,0 +1,121 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oisin on 3/8/2017.
+ */
+public class EmployeeDetailsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void displayRecords() throws Exception {
+
+    }
+
+    @Test
+    public void searchEmployeeById() throws Exception {
+
+    }
+
+    @Test
+    public void searchEmployeeBySurname() throws Exception {
+
+    }
+
+    @Test
+    public void getNextFreeId() throws Exception {
+
+    }
+
+    @Test
+    public void addRecord() throws Exception {
+
+    }
+
+    @Test
+    public void correctPps() throws Exception {
+
+    }
+
+    @Test
+    public void setEnabled() throws Exception {
+
+    }
+
+    @Test
+    public void actionPerformed() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+    @Test
+    public void changedUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void insertUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void removeUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void itemStateChanged() throws Exception {
+
+    }
+
+    @Test
+    public void windowClosing() throws Exception {
+
+    }
+
+    @Test
+    public void windowActivated() throws Exception {
+
+    }
+
+    @Test
+    public void windowClosed() throws Exception {
+
+    }
+
+    @Test
+    public void windowDeactivated() throws Exception {
+
+    }
+
+    @Test
+    public void windowDeiconified() throws Exception {
+
+    }
+
+    @Test
+    public void windowIconified() throws Exception {
+
+    }
+
+    @Test
+    public void windowOpened() throws Exception {
+
+    }
+
+}
